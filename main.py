597146b376b694b4a5c9907ddff85edfd802caa5
gurdeepk <gurdeepk@lentra.ai>
2024-08-16 15:23:58 +0530

    using packages now.

--- main.py
+++ main.py
@@ -3,24 +3,25 @@
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
-import arrays_and_hashing.contains_duplicate
-import arrays_and_hashing.valid_anagram
-import arrays_and_hashing.two_sum
-import arrays_and_hashing.group_anagram
-import arrays_and_hashing.top_k_frequent_elements
-import arrays_and_hashing.product_of_array_except_self
-import arrays_and_hashing.valid_sudoku
-import arrays_and_hashing.longest_consecutive_seq
+from arrays_and_hashing import (contains_duplicate,
+                                longest_consecutive_seq,
+                                valid_anagram,
+                                two_sum,
+                                group_anagram,
+                                top_k_frequent_elements,
+                                product_of_array_except_self,
+                                valid_sudoku)
+
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    # print(arrays_and_hashing.contains_duplicate.solution([1, 2, 3, 1]))
-    # print(arrays_and_hashing.valid_anagram.solution("anagram", "nagaram"))
-    # print(arrays_and_hashing.two_sum.solution([2, 7, 11, 15], 26))
-    # print(arrays_and_hashing.group_anagram.solution(["eat", "tea", "tan", "ate", "nat", "bat"]))
-    # print(arrays_and_hashing.top_k_frequent_elements.solution([1, 1, 1, 2, 2, 3], 2))
-    # print(arrays_and_hashing.product_of_array_except_self.solution([1, 2, 3, 4]))
-    '''print(arrays_and_hashing.valid_sudoku.solution([["5", "3", ".", ".", "7", ".", ".", ".", "."]
+    # print(contains_duplicate.solution([1, 2, 3, 1]))
+    # print(valid_anagram.solution("anagram", "nagaram"))
+    # print(two_sum.solution([2, 7, 11, 15], 26))
+    # print(group_anagram.solution(["eat", "tea", "tan", "ate", "nat", "bat"]))
+    # print(top_k_frequent_elements.solution([1, 1, 1, 2, 2, 3], 2))
+    # print(product_of_array_except_self.solution([1, 2, 3, 4]))
+    '''print(valid_sudoku.solution([["5", "3", ".", ".", "7", ".", ".", ".", "."]
                                 , ["6", ".", ".", "1", "9", "5", ".", ".", "."]
                                 , [".", "9", "8", ".", ".", ".", ".", "6", "."]
                                 , ["8", ".", ".", ".", "6", ".", ".", ".", "3"]
@@ -30,5 +31,5 @@ if __name__ == '__main__':
                                 , [".", ".", ".", "4", "1", "9", ".", ".", "5"]
                                 , [".", ".", ".", ".", "8", ".", ".", "7", "9"]]))
     '''
-    print(arrays_and_hashing.longest_consecutive_seq.solution([100, 200, 5, 1, 3, 2]))
+    print(longest_consecutive_seq.solution([100, 200, 4, 1, 3, 2]))
 
